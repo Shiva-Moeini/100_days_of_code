1bf3a212a693f6f04975cf876ae21f1213fc3d5b
Sheev <shiva.moeini20@gmail.com>
2025-09-20 18:11:34 +0200

    add user input

--- day05_password_generator/password_generator.py
+++ day05_password_generator/password_generator.py
@@ -4,3 +4,10 @@ letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','
 
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ["!", "#", "$", "%", "&","(", ")", "*", "+"]
+
+password = []
+
+print("Welcome to the PyPassword Generator!")
+number_of_letters = int(input("How many letters would you like in your password?\n"))
+number_of_numbers = int(input("How many numbers would you like in your password?\n"))
+number_of_symbols = int(input("How many symbols would you like in your password?\n"))
