5548fe18643a8e1718d5d55cfd8fee0d17e49b59
Sheev <shiva.moeini20@gmail.com>
2025-09-22 18:41:35 +0200

    add input for guessing a letter

--- day07_hangman/hangman.py
+++ day07_hangman/hangman.py
@@ -1,2 +1,9 @@
 import random
 list_of_words = ["python","galaxy","umbrella","mirror","coffee","puzzle","river","castle","rocket","forest","lantern","storm","whisper","cloud","shadow"]
+random_word = random.choice(list_of_words)
+print(random_word)
+word_length = len(random_word)
+print(word_length)
+placeholder = ""
+
+guess = input("Guess a letter:\n").lower()
