6abfd0b2a3adf54bd82e530c8f91fa4995988afc
Sheev <shiva.moeini20@gmail.com>
2025-09-22 20:35:07 +0200

    implement game loop with letter guessing and win condition

--- day07_hangman/hangman.py
+++ day07_hangman/hangman.py
@@ -5,13 +5,27 @@ print(random_word)
 word_length = len(random_word)
 print(word_length)
 placeholder = ""
-display = ""
-for _ in random_word:
+
+for _ in range (word_length):
     placeholder += "-"
 print(placeholder)
+word = []
+game_over = False 
+while not game_over:
+    guess = input("Guess a letter:\n").lower()
+    display = ""
+    for _ in random_word:
+        if _ == guess:
+            display += _
+            word.append(guess)
+        elif _ in word:
+            display += _
+        else:
+            display += "_"
+    print(display)
 
- 
-guess = input("Guess a letter:\n").lower()
-
+    if "_" not in display:
+        game_over = True
+        print("You win!")
 
 
