69b0400651d30bb0e072baeea52d32cdecfc2b89
Sheev <shiva.moeini20@gmail.com>
2025-12-28 23:28:08 +0100

    add input validation for name, bid, and yes/no prompts

--- day09_the_secret_auction/the_secret_auction.py
+++ day09_the_secret_auction/the_secret_auction.py
@@ -4,20 +4,43 @@ information = { }
 
 def winner():
     max_number = max(information , key = information.get)
-    print(f"The winner is {max_number.title()} with bid of {information[max_number]}")
+    print(f"The winner is {max_number.title()} with bid of {information[max_number]}.")
 
 still_bidding = True
+
 while still_bidding:
-    name = input("What is your name?\n").lower()
-    price = int(input("What is your bid?  $"))
+
+    while True:
+        name = input("What is your name?\n").lower()
+        if name.isalpha():
+            break
+        else:
+            print("Name must contain only letters!")
+
+    while True:
+        try:
+            price = int(input("What is your bid? $"))
+            if price > 0:
+                break
+            else:
+                print("Bid must be greater than 0.")
+        except ValueError:
+            print("Not a number!")
+
     information[name] = price
-    print(information)
-    continue_bidding = input("Are there any other bidders? Type 'Yes' or 'No'.\n").lower()
-    if continue_bidding == "yes":
-        print('\n'* 30)
-    elif continue_bidding == "no":
-        still_bidding = False
-        winner()
+    while True:
+        continue_bidding = input("Are there any other bidders? Type 'Yes' or 'No'.\n").lower()
+        if continue_bidding == "yes":
+            print('\n'* 30)
+            break
+        elif continue_bidding == "no":
+            still_bidding = False
+            winner()
+            break
+        else:
+            print("Type 'yes' or 'no'!")
+
+        
 
 
 
