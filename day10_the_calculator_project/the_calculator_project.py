7950075fd1f40476df32d8b895d392a933f60ff3
Sheev <shiva.moeini20@gmail.com>
2025-12-29 21:43:57 +0100

    add calculator loop and restart logic

--- day10_the_calculator_project/the_calculator_project.py
+++ day10_the_calculator_project/the_calculator_project.py
@@ -18,9 +18,24 @@ information = {
     "*" : multiply,
     "/" : devide
 }
-num1 = float(input("Enter the first number:\n"))
 
-for i in information:
-    print(i)
-operation = input("Choose one of the operators.\n")
-num2 = float(input("Enter the second number:\n"))
+def calculator():
+    still_going = True
+    num1 = float(input("Enter the first number:"))
+    while still_going:
+
+        for i in information:
+            print(i)
+        operation = input("Choose one of the operators.")
+        num2 = float(input("Enter the second number:"))
+        result = information[operation](n1=num1,n2=num2)
+        print(result)
+        choice = input(f"Type 'y' to continue with {result} or type 'n' to start a new calculation.")
+        if choice == 'y':
+            num1 = result
+        else:
+            still_going = False
+            print('\n' * 30)
+            calculator()
+
+calculator()
