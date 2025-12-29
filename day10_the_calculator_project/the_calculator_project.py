7389832529feb6b12aabf57736cf62dabd6a2f29
Sheev <shiva.moeini20@gmail.com>
2025-12-29 21:29:04 +0100

    map operators to functions and collect user input

--- day10_the_calculator_project/the_calculator_project.py
+++ day10_the_calculator_project/the_calculator_project.py
@@ -11,3 +11,16 @@ def multiply(n1, n2):
 
 def devide(n1, n2):
     return n1 / n2
+
+information = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : devide
+}
+num1 = float(input("Enter the first number:\n"))
+
+for i in information:
+    print(i)
+operation = input("Choose one of the operators.\n")
+num2 = float(input("Enter the second number:\n"))
