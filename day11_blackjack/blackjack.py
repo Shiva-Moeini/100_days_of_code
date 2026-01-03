a2345df18ad8000736e2d1d548b4f1126a232c68
Sheev <shiva.moeini20@gmail.com>
2026-01-03 13:07:34 +0100

    add calculate_score function

--- day11_blackjack/blackjack.py
+++ day11_blackjack/blackjack.py
@@ -2,10 +2,21 @@ import random
 
 
 def deal_card():
-
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
+
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2 :
+        return 0
+    
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+        
+    return sum(cards)
+
 
 
 user_cards = []
@@ -14,4 +25,7 @@ computer_cards = []
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
+
+
+
 
